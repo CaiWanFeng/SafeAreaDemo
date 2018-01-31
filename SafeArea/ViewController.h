7f0a1b2b905bce35d26cd31987186f8b8ee12527
caiqiang <caiqiang@kuaijiankang.com>
2018-01-31 14:45:22 +0800

    Initial Commit

---
+++ SafeArea/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SafeArea
+//
+//  Created by 蔡强 on 2018/1/31.
+//  Copyright © 2018年 kuaijiankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
